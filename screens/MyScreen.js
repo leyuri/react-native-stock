142e846783f5db1acae79e0d3a2b40303192b7f9
leyuri <leyuri97@gmail.com>
2020-06-12 17:09:28 +0900

    Add MyScreen

---
+++ screens/MyScreen.js
@@ -0,0 +1,15 @@
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import UserInfo from '../components/User/UserInfo'
+
+
+export default function LinksScreen() {
+  return (
+    <ScrollView >
+      <UserInfo />
+    </ScrollView>
+  );
+}

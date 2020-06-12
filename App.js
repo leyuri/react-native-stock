9b1add1e229d1af65db0d51cd553eef48431aaa9
leyuri <leyuri97@gmail.com>
2020-06-12 17:03:03 +0900

    Add store usnig react-redux

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/index';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// store 만들어주기
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(applyMiddleware(logger,thunk)));
 
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+export default class App extends React.Component {
+  render() {
+      return (
+        <Provider store={store}>
+          <View style={styles.container}>
+            <NavigationContainer linking={LinkingConfiguration}>
+              <Stack.Navigator>
+                <Stack.Screen name="Root" component={BottomTabNavigator} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </View>
+        </Provider>
+      );
+    }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,

90e674297252673b826fd9d65edeed901f5cc9e0
leyuri <leyuri97@gmail.com>
2020-06-13 15:10:48 +0900

    Add StockSearch ui

---
+++ components/Stock/StockSearch.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+export default class SearchBarExample extends Component {
+  render() {
+    return (
+
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+            <Icon name="ios-pulse" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+
+    );
+  }
+}

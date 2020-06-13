b591fda39fbe72f5f56db200b51d2e21b676d1ec
leyuri <leyuri97@gmail.com>
2020-06-14 02:02:32 +0900

    Change NewsList UI

--- components/News/NewsList.js
+++ components/News/NewsList.js
@@ -1,37 +1,116 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
-export default class CardShowcaseExample extends Component {
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+
+
+const apple = require("../../assets/images/apple.png")
+const amazon = require("../../assets/images/amazon.jpeg")
+const sumsun = require("../../assets/images/sumsun.png")
+
+const sample = require("../../assets/images/stock.png")
+// const profile = require("../../assets/images/profile.png")
+
+
+export default class CardImageExample extends Component {
   render() {
     return (
-
-          <Card style={{flex: 0}}>
+      <Container>
+        <Content>
+          <Card>
             <CardItem>
               <Left>
-                <Thumbnail source={{uri: 'Image URL'}} />
+                <Thumbnail source={apple} />
                 <Body>
                   <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
+                  <Text note>GeekyAnts</Text>
                 </Body>
               </Left>
             </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
-                <Text>
-                  //Your text here
-                </Text>
-              </Body>
+            <CardItem cardBody>
+              <Image source={sample} style={{height: 200, width: null, flex: 1}}/>
             </CardItem>
             <CardItem>
               <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Icon name="logo-github" />
-                  <Text>1,926 stars</Text>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>91 Likes</Text>
                 </Button>
               </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
             </CardItem>
           </Card>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={amazon} />
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>GeekyAnts</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={sample} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>82Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>98 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={sumsun} />
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>GeekyAnts</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={sample} style={{height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }

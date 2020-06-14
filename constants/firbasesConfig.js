1118a8130ac5636b99b62800d1b79a1c13aa5e5b
leyuri <leyuri97@gmail.com>
2020-06-14 12:43:07 +0900

    Add firbases key

---
+++ constants/firbasesConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0A2z_hvoZqhPLt9gEOcT6OvsiUsiV-hY",
+    authDomain: "react-native-stock.firebaseapp.com",
+    databaseURL: "https://react-native-stock.firebaseio.com",
+    projectId: "react-native-stock",
+    storageBucket: "react-native-stock.appspot.com",
+    messagingSenderId: "552409501914",
+    appId: "1:552409501914:web:3e1edf5d3e6de5029f55e3",
+    measurementId: "G-DFBXR3Z6CW"
+  };
